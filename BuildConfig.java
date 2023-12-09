5eb97d37808f46adc3c025e61e9bc63ed7ab1c73
Sarah <ssarshine@gmail.com>
2023-12-09 13:11:39 +0700

    first commit

---
+++ BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.projectsarah4b;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.projectsarah4b";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
